6c50448c257e619dbaec5c9399f3699051bdbcdd
HanielDorton <danhorton0@gmail.com>
2014-03-07 11:07:36 -0800

    8

--- src/com/haniel/rain/Game.java
+++ src/com/haniel/rain/Game.java
@@ -10,6 +10,8 @@ import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 
+import com.haniel.rain.graphics.Screen;
+
 public class Game extends Canvas implements Runnable{
 	
 	/**
@@ -24,6 +26,8 @@ public class Game extends Canvas implements Runnable{
 	private JFrame frame;
 	private boolean running = false;
 	
+	private Screen screen;
+	
 	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);	
 	private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
 	
@@ -31,7 +35,10 @@ public class Game extends Canvas implements Runnable{
 		Dimension size = new Dimension(width*scale, height*scale);
 		setPreferredSize(size);
 		
+		screen = new Screen(width, height);
+		
 		frame = new JFrame();
+		
 	}
 	
 	public synchronized void start() {
