0b23c6e358fde77bbb4d2ae9971ebac54bfb2ce4
HanielDorton <danhorton0@gmail.com>
2014-03-11 17:43:25 -0700

    19

--- src/com/haniel/rain/graphics/Screen.java
+++ src/com/haniel/rain/graphics/Screen.java
@@ -3,10 +3,12 @@ package com.haniel.rain.graphics;
 import java.util.Random;
 
 public class Screen {
+	
 	private int width, height;
 	public int[] pixels;
-	public final int MAP_SIZE = 8;
+	public final int MAP_SIZE = 64;
 	public final int MAP_SIZE_MASK = MAP_SIZE - 1;
+	
 	public int[] tiles = new int[MAP_SIZE * MAP_SIZE];
 	
 	private Random random = new Random();
