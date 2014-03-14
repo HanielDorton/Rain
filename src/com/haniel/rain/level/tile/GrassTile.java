f8eb46dc2b62c1a50cf92e39c51eb1262920a399
HanielDorton <danhorton0@gmail.com>
2014-03-13 17:17:55 -0700

    27

---
+++ src/com/haniel/rain/level/tile/GrassTile.java
@@ -0,0 +1,15 @@
+package com.haniel.rain.level.tile;
+
+import com.haniel.rain.graphics.Screen;
+import com.haniel.rain.graphics.Sprite;
+
+public class GrassTile extends Tile{
+
+	public GrassTile(Sprite sprite) {
+		super(sprite);
+	}
+	
+	public void render(int x, int y, Screen screen) {
+		//Do render stuff here
+	}
+}
