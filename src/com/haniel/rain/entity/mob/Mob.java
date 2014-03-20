e44ac04e0ed3f9cf5494230d852643aef4181bd3
HanielDorton <danhorton0@gmail.com>
2014-03-20 13:26:15 -0700

    40

---
+++ src/com/haniel/rain/entity/mob/Mob.java
@@ -0,0 +1,23 @@
+package com.haniel.rain.entity.mob;
+
+import com.haniel.rain.entity.Entity;
+import com.haniel.rain.graphics.Sprite;
+
+
+public abstract class Mob extends Entity{
+	
+	protected Sprite sprite;
+	protected int dir = 0;
+	protected boolean moving = false;
+	
+	public void move() {
+	}
+	
+	public void update() {
+	}
+	
+	private boolean collision () {
+		return false;
+	}
+
+}
