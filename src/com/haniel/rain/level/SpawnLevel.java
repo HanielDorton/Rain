375cf90ec26c20def33300c366706783eff86bd2
HanielDorton <danhorton0@gmail.com>
2014-03-22 15:55:23 -0700

    53

--- src/com/haniel/rain/level/SpawnLevel.java
+++ src/com/haniel/rain/level/SpawnLevel.java
@@ -21,14 +21,23 @@ public class SpawnLevel extends Level{
 			BufferedImage image = ImageIO.read(SpawnLevel.class.getResource(path));
 			int w = image.getWidth();
 			int h = image.getHeight();
+			tiles = new Tile[w * h];
 			image.getRGB(0, 0, w, h, levelPixels, 0, w);
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.out.println("Exception! Could not load level file");		
 		}
 	}
-	protected void generateLevel() {
 	
+	// Grass = 0xFF00
+	// Flower = 0xFFFF00
+	// Rock = 0x7F7F00
+	protected void generateLevel() {
+		for (int i = 0; i < levelPixels.length; i++) {
+			if (levelPixels[i] == 0xFF00) tiles[i] = Tile.grass;
+			if (levelPixels[i] == 0xFFFF00) tiles[i] = Tile.flower;
+			if (levelPixels[i] == 0x7F7F00) tiles[i] = Tile.rock;
+		}
 	}
 
 }
