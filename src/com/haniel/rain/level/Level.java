04eca42143d24d96329b1b955d65abfba23372c6
HanielDorton <danhorton0@gmail.com>
2014-03-23 08:19:47 -0700

    55

--- src/com/haniel/rain/level/Level.java
+++ src/com/haniel/rain/level/Level.java
@@ -12,12 +12,13 @@ public class Level {
 	public Level(int width, int height) {
 		this.width = width;
 		this.height = height;
-		tiles = new int[width * height];;
+		tilesint = new int[width * height];
 		generateLevel();
 	}
 	
 	public Level(String path) {
 		loadLevel(path);
+		generateLevel();
 	}
 	
 	protected void generateLevel() {
@@ -44,8 +45,11 @@ public class Level {
 		for (int y = y0; y < y1; y++) {
 			for (int x = x0; x < x1; x++) {
 				//getTile(x, y).render(x, y, screen);
-				if (x < 0 || y < 0 || x >= width || y >= height) Tile.voidTile.render(x, y, screen);
-				else tiles[x + y * 16].render(x, y, screen);
+				if (x + y * 16 < 0 || x + y * 16 >= 256) {
+					Tile.voidTile.render(x, y, screen);
+					continue;
+				}
+				tiles[x + y * 16].render(x, y, screen);
 				
 			}
 		}
