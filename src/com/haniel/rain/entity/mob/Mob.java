9f5c2404c608de9eb8145a739ce3f7762b406775
HanielDorton <danhorton0@gmail.com>
2014-03-25 13:35:17 -0700

    64

--- src/com/haniel/rain/entity/mob/Mob.java
+++ src/com/haniel/rain/entity/mob/Mob.java
@@ -10,14 +10,19 @@ public abstract class Mob extends Entity{
 	protected boolean moving = false;
 	
 	public void move(int xa, int ya) {
+		if (xa != 0 && ya != 0) {
+			move(xa, 0);
+			move(0, ya);
+			return;
+		}
 		if (xa > 0) dir = 1;
 		if (xa < 0) dir = 3;
 		if (ya > 0) dir = 2;
 		if (ya < 0) dir = 0;
 		
 		if (!collision(xa, ya)) {
-			x += xa;
 			y += ya;
+			x += xa;
 		}
 	}
 	
