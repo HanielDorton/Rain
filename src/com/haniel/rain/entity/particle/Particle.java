ff72758228955e37122a62af6316722fbf1e1998
HanielDorton <danhorton0@gmail.com>
2014-04-04 11:44:46 -0700

    80

--- src/com/haniel/rain/entity/particle/Particle.java
+++ src/com/haniel/rain/entity/particle/Particle.java
@@ -11,10 +11,10 @@ public class Particle extends Entity{
 	private int life;
 	private int time = 0;
 	
-	protected double xx, yy, xa, ya;
+	protected double xx, yy, zz;
+	protected double xa, ya, za;
 	
 	public Particle(int x, int y, int life) {
-		System.out.println("Particle Life " + life);
 		this.x = x;
 		this.y = y;
 		this.xx = x;
@@ -22,21 +22,34 @@ public class Particle extends Entity{
 		this.life = life + (random.nextInt(40) - 20);
 		sprite = Sprite.particle_normal;
 		
-		this.xa = random.nextGaussian();
+		this.xa = random.nextGaussian() + 1.8;
+		if (this.xa < 0) xa = 0.1;
 		this.ya = random.nextGaussian();
+		this.zz = random.nextFloat() + 2.0;
 	}
 	
 	public void update() {
 		time++;
 		if (time > 7400) time = 0;
 		if (time > life) remove();
+		za -= 0.1;
+		
+		if (zz < 0) {
+			zz = 0;
+			za *= -0.6;
+			xa *= 0.4;
+			ya *= 0.4;
+		}
+		
+		
 		this.xx += xa;
 		this.yy += ya;
+		this.zz += za;
 		
 	}
 	
 	public void render(Screen screen) {
-		screen.renderSprite((int)xx, (int)yy, sprite, true);
+		screen.renderSprite((int)xx - 12, (int)yy - (int) zz, sprite, true);
 	}
 
 }
--- src/com/haniel/rain/entity/projectile/WizardProjectile.java
+++ src/com/haniel/rain/entity/projectile/WizardProjectile.java
@@ -21,7 +21,7 @@ public class WizardProjectile extends Projectile{
 	
 	public void update() {
 		if (level.tileCollision(x, y, nx, ny, 7)) {
-			level.add(new ParticleSpawner((int)x, (int)y, 44, 50, level));
+			level.add(new ParticleSpawner((int)x, (int)y, 80, 50, level));
 			remove();
 		}
 		move();
